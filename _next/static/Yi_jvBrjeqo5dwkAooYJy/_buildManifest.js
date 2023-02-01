34c04ac5d2c8e6f2e2a7d2149f68f6192950160a
sutan mufti <upsutan@gmail.com>
2023-02-01 00:33:56 +0000

    build 2 with paragraph@

--- _next/static/Yi_jvBrjeqo5dwkAooYJy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST=function(a){return{__rewrites:{beforeFiles:[],afterFiles:[{source:"/coffee",destination:a}],fallback:[]},"/":["static/chunks/pages/index-88973aeaf563430b.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:[a,"/_app","/_error"]}}("/"),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
